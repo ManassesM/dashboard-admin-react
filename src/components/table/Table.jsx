21c10dbc77ae82060ea1a4307aef5637d4c840d1
ManassesM <manamiui2001@gmail.com>
2021-11-30 14:07:30 -0300

    pagination

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './table.css'
 
 const Table = (props) => {
+	const iniDataShow =
+		props.limit && props.bodyData
+			? props.bodyData.slice(0, Number(props.limit))
+			: props.bodyData
+
+	const [dataShow, setDataShow] = useState(iniDataShow)
+	const [currPage, setCurrPage] = useState(0)
+
+	const slectPage = (page) => {
+		const start = Number(props.limit) * page
+		const end = start + Number(props.limit)
+
+		setDataShow(props.bodyData.slice(start, end))
+		setCurrPage(page)
+	}
+
+	let pages = 1
+	let range = []
+
+	if (props.limit !== undefined) {
+		let page = Math.floor(props.bodyData.length / Number(props.limit))
+		pages =
+			props.bodyData.length % (Number(props.limit) === 0) ? page : page + 1
+		range = [...Array(pages).keys()]
+	}
+
 	return (
 		<div>
 			<div className='table-wrapper'>
@@ -19,12 +45,29 @@ const Table = (props) => {
 						) : null}
 						{props.bodyData && props.renderBody ? (
 							<tbody>
-								{props.bodyData.map((item, idx) => props.renderBody(item, idx))}
+								{dataShow.map((item, idx) => props.renderBody(item, idx))}
 							</tbody>
 						) : null}
 					</thead>
 				</table>
 			</div>
+			{pages > 1 ? (
+				<div className='table__pagination'>
+					{range.map((item, idx) => (
+						<div
+							key={idx}
+							className={`table__pagination-item ${
+								currPage === idx ? 'active' : ''
+							}`}
+							onClick={() => slectPage(idx)}
+						>
+							{item + 1}
+						</div>
+					))}
+				</div>
+			) : (
+				''
+			)}
 		</div>
 	)
 }
--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,54 @@
+import React from 'react'
+
+import Table from '../components/table/Table'
+
+import customerList from '../assets/JsonData/customers-list.json'
+
+const customerTableHead = [
+	'',
+	'name',
+	'email',
+	'phone',
+	'total orders',
+	'total spend',
+	'location',
+]
+
+const renderHead = (item, idx) => <th key={idx}>{item}</th>
+
+const renderBody = (item, idx) => (
+	<tr key={idx}>
+		<td>{item.id}</td>
+		<td>{item.name}</td>
+		<td>{item.email}</td>
+		<td>{item.phone}</td>
+		<td>{item.total_orders}</td>
+		<td>{item.total_spend}</td>
+		<td>{item.location}</td>
+	</tr>
+)
+
 const Customers = () => {
-  return (
-    <div>
-      Customers
-    </div>
-  )
+	return (
+		<div>
+			<h2 className='page-header'>Customers</h2>
+			<div className='row'>
+				<div className='col-12'>
+					<div className='card'>
+						<div className='card__body'>
+							<Table
+								limit='10'
+								headData={customerTableHead}
+								renderHead={(item, idx) => renderHead(item, idx)}
+								bodyData={customerList}
+								renderBody={(item, idx) => renderBody(item, idx)}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Customers
